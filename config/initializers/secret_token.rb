eb9e638bb5dba053bc566d93fc38aa8a21d07278
kevin <arinspect@gmail.com>
2015-09-11 23:27:30 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Unleashedantics::Application.config.secret_key_base = '32d8f7bf05ff66aca755e786c9df4b2d13c50dfbce5428e83eeb5775492c6c2c0d02c8d6eed2bc69877a0891c81203138016b9362673efab85909c17d6470948'
