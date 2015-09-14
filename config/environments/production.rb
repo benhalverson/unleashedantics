d6cd09e7dcec4351c8ede28cf41efbf3ccdaa7b4
kevin <arinspect@gmail.com>
2015-09-13 19:59:36 -0500

    added devise on day 14 lesson 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,14 @@ Unleashedantics::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-end
+
+  #required for Heroku
+  #Note to set this to your actual host
+  config.action_mailer.default_url_options = { :host => 'unleashedantics.com/' }
+
+
+
+
+
+
+
